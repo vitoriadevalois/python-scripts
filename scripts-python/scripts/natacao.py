6fa74edcc99af9acbe0fda3bcbd3f200bad32c8e
Vitória de Valois <81873935+vitoriadevalois@users.noreply.github.com>
2021-06-14 21:22:28 -0300

    updates

---
+++ scripts-python/scripts/natacao.py
@@ -0,0 +1,17 @@
+#Programa que lê o ano de nascimento de uma tleta e mostra a categoria de acordo com a idade.
+
+from datetime import date
+atual = date.today().year
+nascimento = int(input('Ano de Nascimento: '))
+idade = atual - nascimento
+print('O atleta tem {} anos.'.format(idade))
+if idade <= 9:
+    print('Classificação: MIRIM')
+elif idade <= 14:
+    print('Classificação: INFANTIL')
+elif idade <= 19:
+    print('Classificação: JUNIOR')
+elif idade <= 25:
+    print('Classificação: SÊNIOR')
+else:
+    print('Classificação: MASTER')

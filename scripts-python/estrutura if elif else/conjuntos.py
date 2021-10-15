b12fe1ce3da86566aef963d05cc401c459ce3e30
Vitória de Valois <81873935+vitoriadevalois@users.noreply.github.com>
2021-10-15 17:49:55 -0300

    updates

---
+++ scripts-python/estrutura if elif else/conjuntos.py
@@ -0,0 +1,6 @@
+#Organizando conjntos e subconjuntos de elementos em Python
+
+conjunto = {1, 2, 3, 4, 4, 2}
+conjunto.add(5) #para adicionar um elemento no conjunto
+conjunto.discard(2) #para remover um elemento no conjunto
+print(conjunto)

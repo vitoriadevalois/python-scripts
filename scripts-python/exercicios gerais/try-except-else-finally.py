2d86e5e93758e8001114eda6e6655b2a0a0eb880
vitoriadevalois <vitoriadvalois@gmail.com>
2021-11-08 20:33:38 -0300

    update try-except-else-finally.py

--- scripts-python/exercicios gerais/try-except-else-finally.py
+++ scripts-python/exercicios gerais/try-except-else-finally.py
@@ -1,13 +1,27 @@
-# Gerenciando e criando exceções customizadas com try, except, else e finally
+# Gerenciando e criando exceções customizadas com try, except, else e finally.
+# Consulte https://docs.python.org/3/library/exceptions.html#exception-context para saber a hierarquia das exceções (exception hierarchy)
 
 lista = [1, 10]
+
 try:
+    arquivo = open('teste.txt', 'r')
+    texto = arquivo.read()
     divisao = 10 / 1
     numero = lista[1]
-    x = a
+    print('fechando arquivo')
+    arquivo.close()
+    
 except ZeroDivisionError:
-    print('Não é possível realizar uma divisão por 0')
+    print('Não é possível realizar divisão por zero.')   
+except ArithmeticError:
+    print('Houve um erro ao realizar uma operação aritmética')
 except IndexError:
     print('Erro ao acessar um índice inválido')
 except BaseException as ex:
     print('Erro desconhecido. Erro: {}'.format(ex))
+else:
+    print('Executa quando não ocorre exceção')
+finally:
+    print('Sempre executa')
+    print('Fechando arquivo')
+    arquivo.close()
